bc41357703fb46ec34fff8422e0bac18facd0860
Pob <590650@gmail.com>
2019-08-21 15:09:55 +0700

    add logic to prevent scroll to top-most and bottom-most

--- script.js
+++ script.js
@@ -11,3 +11,11 @@ document.querySelector('.modal').addEventListener('touchmove', e => {
   console.log('modal scrolling')
   e.stopPropagation()
 })
+
+const modal = document.querySelector('.modal').addEventListener('scroll', () => {
+  if (modal.scrollTop < 1) {
+    modal.scrollTop = 1
+  } else if (modal.scrollHeight - modal.scrollTop - modal.clientHeight < 1) {
+    modal.scrollTop = modal.scrollTop - 1
+  }
+})
